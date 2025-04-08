2aa07f7511050414c341a88615fa671d6e626f49
Ololade Olaonipekun <olaonipekunololade94@gmail.com>
2025-04-08 17:40:56 +0100

    learnt something new - blog post and content

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import auth from 'auth-astro';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [auth()]
 });
